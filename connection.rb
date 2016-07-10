9357a491acbb9fde53e79b1ff44ad80e221b6915
brahian609 <brahian609@hotmail.com>
2016-07-10 15:06:16 -0500

    feat(connection) convert to class

--- connection.rb
+++ connection.rb
@@ -2,16 +2,35 @@
 
 require 'mysql2'
 
-begin
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "mysql")
-  results = client.query("SELECT * FROM user WHERE Host='localhost'")
+class Connection_mysql
 
-  results.each do |row|
-    puts row["User"]
+  @db = ""
+
+  def initialize
+
+    begin
+      @db = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "mysql")
+      results = @db.query("SELECT * FROM user WHERE Host='localhost'")
+
+      results.each do |row|
+        puts row["User"]
+      end
+
+    rescue Mysql2::Error => e
+      puts e.errno
+      puts e.error
+
+    end
+
   end
 
-rescue Mysql2::Error => e
-  puts e.errno
-  puts e.error
+  def test_connection
+
+    puts @db.query("select version()")
+
+  end
 
 end
+
+mysql = Connection_mysql.new()
+mysql.test_connection
